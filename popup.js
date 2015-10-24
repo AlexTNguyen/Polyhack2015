64da3cb69470d1681b4b369f1e6bb03163509901
npham02 <nga.pham@tufts.edu>
2015-10-24 01:49:15 -0400

    working version of entering into text box

--- popup.js
+++ popup.js
@@ -1,35 +1,43 @@
-/*
-$(function(){
-  $('#paste').click(function(){pasteSelection();});
-});
 
+/*
 function pasteSelection() {
-	console.log("inside pasteSelection");
   	chrome.tabs.query({"active":true, "currentWindow": true}, 
 	  	function(tab) {
 	    chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
 	    	function (response) {
-	     	var text = document.getElementById('text'); 
-	    	text.innerHTML = "hello" + response.data;
+	    	console.log(tab)
+	    	console.log(response)
 	    });
 	});
+}
+*/
+
+
+function UrbanDictDefinition(data, text){
+	var xhr = new XMLHttpRequest(); 
+	var link = "http://api.urbandictionary.com/v0/define?term=" + data; 
+	xhr.open("GET", link, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			parsedJSON = JSON.parse(xhr.responseText);
+			text.value = parsedJSON["list"][0]["definition"];
+		} else {
+			text.value = "Not found"; 
+		}
+	}
+	xhr.send();
+}
+
+
+function pasteSelection() {
+    var text = document.getElementById('text'); 
+   	UrbanDictDefinition(text.value, text);
 }
 
 document.getElementById('btn').addEventListener('click', function() {
 	pasteSelection();
 })
-*/
 
-$(function(){
-  $('#paste').click(function(){pasteSelection();});
-});
-function pasteSelection() {
-  chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
-  function(tab) {
-    chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
-    function(response){
-      var text = document.getElementById('text'); 
-      text.innerHTML = response.data;
-    });
-  });
-}
+
+
+
